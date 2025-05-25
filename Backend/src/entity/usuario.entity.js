694d95166687274e364c2f48d7d433bdf24760fa
Tomas Saez <tomas.saez2101@alumnos.ubiobio.cl>
2025-05-25 15:31:37 -0400

    ADD entidad usuario y servicio

---
+++ Backend/src/entity/usuario.entity.js
@@ -0,0 +1,69 @@
+"use strict"
+import { EntitySchema } from "typeorm";
+
+const Usuarios = new EntitySchema({
+    name: "Usuario",
+    //nombre de la tabla en la base de datos
+    tableName: "usuarios",
+    columns: {
+        id_usuario: {
+            primary: true,
+            type: "int",
+            generated: true,
+        },
+        nombreCompleto: { 
+            type: "varchar",
+            length: 100,
+            nullable: false,
+        },
+        email: {
+            type: "varchar",
+            length: 100,
+            unique: true,
+            nullable: false,
+        },
+        telefono: {
+            type: "varchar",
+            length: 15,
+            nullable: true,
+        },
+        password: {
+            type: "varchar",
+            length: 20,
+            nullable: false,
+        },
+        rol: {
+            type: "enum",
+            enum: ["admin", "cliente"],
+            default: "cliente",
+            nullable: false,
+        },
+        createdAt: {
+            type: "timestamp",
+            default: () => "CURRENT_TIMESTAMP",
+            nullable: false,
+        },
+        updatedAt: {
+            type: "timestamp",
+            default: () => "CURRENT_TIMESTAMP",
+            onUpdate: "CURRENT_TIMESTAMP",
+            nullable: false,
+        },
+    },
+
+    indices: [
+        {
+            name: "IDX_USER",
+            columns: ["id_cliente"],
+            unique: true,
+        },
+
+        {
+            name: "IDX_EMAIL",
+            columns: ["email"],
+            unique: true,
+        },
+    ],
+});
+
+export default Usuarios;
