619fd7fa9aae46837d762bb143108965df135ca4
Tomas Saez <tomas.saez2101@alumnos.ubiobio.cl>
2025-06-11 23:24:31 -0400

    FIX authContext

--- Frontend/src/context/AuthContext.jsx
+++ Frontend/src/context/AuthContext.jsx
@@ -1,20 +1,27 @@
-import { createContext, useContext, } from "react";
+import { createContext, useContext,useState } from "react";
 
+// crea un contexto que usaremos para manejar la autenticación
 const AuthContext = createContext();
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const useAuth = () => useContext(AuthContext);
-
-export function AuthProvider({ children }) {
-  //obtiene info del sessionStorage
-  const user = JSON.parse(sessionStorage.getItem("usuario")) || "";
-  const isAuthenticated = user ? true : false;
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
 
+export function AuthProvider(props) {
+  const [authUser, setAuthUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const value = {
+    authUser,
+    setAuthUser,
+    isAuthenticated,
+    setIsAuthenticated,
+  }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, user }}>
-      {children}
+    <AuthContext.Provider value={value}>
+      {props.children}
     </AuthContext.Provider>
   );
 }
